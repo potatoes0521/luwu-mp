d9c179ec569d9b5fbb2b9c42209092ec5b7299a7
liuyang <liuy@paoche56.com>
2020-06-17 17:33:36 +0800

    用户数据

---
+++ src/services/modules/user.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: liuYang
+ * @description: 用户信息
+ * @path: 引入路径
+ * @Date: 2020-06-17 15:42:56
+ * @LastEditors: liuYang
+ * @LastEditTime: 2020-06-17 16:41:21
+ * @mustParam: 必传参数
+ * @optionalParam: 选传参数
+ * @emitFunction: 函数
+ */ 
+import request from "../request"
+
+/**
+* 获取首页列表数据
+* @return void
+*/
+// eslint-disable-next-line import/prefer-default-export
+export const userLogin = (data, that) => { 
+  return request.post(`user/v1/user/login/wechat`, data, that, false)
+}
