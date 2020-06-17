4b820e48c3ac58e7adc1f917ddc7014fb18e91e8
liuyang <liuy@paoche56.com>
2020-06-17 10:40:15 +0800

    开发配置

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line import/no-commonjs
+const path = require('path')
+
 module.exports = {
   env: {
     NODE_ENV: '"production"'
@@ -14,5 +17,17 @@ module.exports = {
      *     .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin, [])
      * }
      */
-  }
+  },
+  alias: {
+    '@services': path.resolve(__dirname, '..', 'src/services'),
+    '@components': path.resolve(__dirname, '..', 'src/components'),
+    '@containers': path.resolve(__dirname, '..', 'src/containers'),
+    '@utils': path.resolve(__dirname, '..', 'src/utils'),
+    '@assets': path.resolve(__dirname, '..', 'src/assets'),
+    '@lib': path.resolve(__dirname, '..', 'src/lib'),
+    '@constant': path.resolve(__dirname, '..', 'src/constant'),
+    '@store': path.resolve(__dirname, '..', 'src/store'),
+    '@shared': path.resolve(__dirname, '..', 'src/shared'),
+    '@@cdn': path.resolve(__dirname, '..', 'src/constant/cdn_assets.js')
+  },
 }
