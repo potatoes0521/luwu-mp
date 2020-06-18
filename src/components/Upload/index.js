6855863532e1dabbab0e38affeee46255062d4bc
liuyang <liuy@paoche56.com>
2020-06-18 18:13:08 +0800

    优化横向滚动组件对齐方式

--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -4,13 +4,12 @@
  * @path: 引入路径
  * @Date: 2020-06-17 17:35:56
  * @LastEditors: liuYang
- * @LastEditTime: 2020-06-18 14:19:48
+ * @LastEditTime: 2020-06-18 18:12:22
  * @mustParam: 必传参数
  *  imageList 要展示的图片
  * @optionalParam: 选传参数
  *  addBtnSizeType 添加图片大小
  *  imageSizeType 图片大小
- *  alignType 垂直对齐方式
  *  showAddBtn 是否展示上传图片
  * @emitFunction: 函数
  *  onUploadOK 图片上传完毕
@@ -30,14 +29,9 @@ import './index.scss'
 
 export default class Upload extends Component { 
 
-  constructor(props) {
-    super(props)
-  }
-
-
   componentDidMount() {
     if (this.props.autoChoose) { 
-      this.chooseImage()
+      // this.chooseImage()s
     }
   }
 
@@ -70,65 +64,56 @@ export default class Upload extends Component {
       imageList,
       addBtnSizeType,
       imageSizeType,
-      alignType,
       showAddBtn,
-      scrollViewWidth
+      computedWidth
     } = this.props
     // 图片展示列表
-    const imageListRender = imageList.map((item) => {
-      const imageItemClassName = classNames('picture-item', {
-        'big-picture-item': imageSizeType === 'big' || !imageSizeType,
-        'small-picture-item': imageSizeType === 'small'
-      })
-      return (
-        <View
-          className={imageItemClassName}
-          key={item}
-        >
-          {/* <Text
-            onClick={() => deleteImage('car', index)}
-            className='delete-btn iconfont iconguanbi2'
-          ></Text> */}
-          <Image 
-            mode='aspectFill'
-            className='image' 
-            src={item}
-            onClick={() => this.showImage(item)}
-          ></Image>
-        </View>
-      )
+    const imageItemClassName = classNames('picture-item', {
+      'big-picture-item': imageSizeType === 'big' || !imageSizeType,
+      'small-picture-item': imageSizeType === 'small'
     })
     // 添加图片按钮
     const addBtnClassName = classNames('add-btn-public', {
       'small-btn': addBtnSizeType === 'small' || !addBtnSizeType,
       'big-btn': addBtnSizeType === 'big'
     })
-    // 列表垂直方式
-    const listClassName = classNames('picture-list', {
-      'align-center': alignType === 'center' || !alignType,
-      'flex-end': alignType === 'flex-end'
-    })
+    const imageListRender = imageList.map((item) => (
+      <View
+        className={imageItemClassName}
+        key={item}
+      >
+        <Image 
+          mode='aspectFill'
+          className='image' 
+          src={item}
+          onClick={() => this.showImage(item)}
+        ></Image>
+      </View>
+    ))
+    const height = imageSizeType === 'small' ? 130 : 180
     return (
     <ScrollView
       scrollX
-      style={{width: `calc(100vw - ${scrollViewWidth}rpx)`}}
+      style={{
+          width: `calc(100vw - ${computedWidth}rpx)`,
+          height: `${height}rpx`
+        }}
       className='scroll-view'
     >
-      <View className={listClassName}>
-        {
-          imageListRender
-        }
-        {
-          showAddBtn && (
-            <View
-              className={addBtnClassName}
-              onClick={this.chooseImage.bind(this)}
-            >
-              <Text className='iconfont icontuxiang icon-upload-img'></Text>
-            </View>
-          )
-        }
-      </View>
+      {
+        imageListRender
+      }
+      {
+        showAddBtn && (
+          <View
+            className={addBtnClassName}
+            onClick={this.chooseImage.bind(this)}
+          >
+            <Text className='iconfont iconbianzu icon-upload-img'></Text>
+          </View>
+        )
+      }
+      {imageList && imageList.length < 1 && <View className={imageItemClassName}></View>}
     </ScrollView>
   )
   }
@@ -140,7 +125,6 @@ Upload.defaultProps = {
   autoChoose: false,
   addBtnSizeType: 'small',
   imageSizeType: 'small',
-  alignType: 'center',
   showAddBtn: false,
   scrollViewWidth: 0,
   onUploadOK: () => {
@@ -153,6 +137,5 @@ Upload.propTypes = {
   showAddBtn: PropTypes.bool,
   addBtnSizeType: PropTypes.string,
   imageSizeType: PropTypes.string,
-  alignType: PropTypes.string,
   onUploadOK: PropTypes.func.isRequired
 }
