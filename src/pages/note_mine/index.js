f8f22ae0e77cf4305c473731ab3857b6744d5a71
liuyang <liuy@paoche56.com>
2020-06-19 09:31:25 +0800

    我发布的列表

--- src/pages/note_mine/index.js
+++ src/pages/note_mine/index.js
@@ -4,7 +4,7 @@
  * @path: 引入路径
  * @Date: 2020-06-17 11:08:36
  * @LastEditors: liuYang
- * @LastEditTime: 2020-06-17 11:14:09
+ * @LastEditTime: 2020-06-19 09:30:00
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  * @emitFunction: 函数
@@ -13,7 +13,8 @@ import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
 import classNames from 'classnames'
 import { connect } from '@tarojs/redux'
-import api from '@api/index.js'
+import Location from '@components/Location'
+import SaveAreaView from '@components/SafeAreaView'
 
 import './index.scss'
 
@@ -28,12 +29,61 @@ class index extends Component {
   }
 
   config = {
-    navigationBarTitleText: '我的笔记' 
+    navigationBarTitleText: '我的笔记',
+    navigationStyle: 'custom'
   }
 
   render() {
+    const noteListRender = [1, 2].map(item => {
+      return (
+        <View className='note-item' key={item}>
+          <Location onlyShow />
+          <View className='note-main'>
+            <View className='note-line-details'>
+              <View className='note-detail-item'>
+                <View className='note-detail-item-label'>品类</View>
+                <View className='note-detail-item-content'>1111</View>
+              </View>
+              <View className='note-detail-item'>
+                <View className='note-detail-item-label'>品牌</View>
+                <View className='note-detail-item-content'>1111</View>
+              </View>
+            </View>
+            <View className='note-line-details'>
+              <View className='note-detail-item'>
+                <View className='note-detail-item-label'>价格</View>
+                <View className='note-detail-item-content'>111</View>
+              </View>
+              <View className='note-detail-item'>
+                <View className='note-detail-item-label'>型号</View>
+                <View className='note-detail-item-content'>1111</View>
+              </View>
+            </View>
+            <View className='note-line-details'>
+              <View className='note-detail-item'>
+                <View className='note-detail-item-label'>记录时间</View>
+                <View className='note-detail-item-content'>11111</View>
+              </View>
+            </View>
+            <View className='upload'>
+              
+            </View>
+          </View>
+        </View>
+      )
+    })
     return (
-      <View></View>
+      <SaveAreaView
+        title='的笔记'
+        back
+        home
+      >
+        <View className='page-wrapper'>
+          {
+            noteListRender
+          }
+        </View>
+      </SaveAreaView>
     )
   }
 
