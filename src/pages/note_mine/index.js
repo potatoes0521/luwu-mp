85fa66c762effabbabe9f96a6525141788b3d7fe
liuyang <liuy@paoche56.com>
2020-06-19 10:15:27 +0800

    优化

--- src/pages/note_mine/index.js
+++ src/pages/note_mine/index.js
@@ -4,18 +4,17 @@
  * @path: 引入路径
  * @Date: 2020-06-17 11:08:36
  * @LastEditors: liuYang
- * @LastEditTime: 2020-06-19 09:30:00
+ * @LastEditTime: 2020-06-19 09:53:00
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  * @emitFunction: 函数
  */ 
 import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
-import classNames from 'classnames'
 import { connect } from '@tarojs/redux'
-import Location from '@components/Location'
 import SaveAreaView from '@components/SafeAreaView'
-
+import NoteItem from './components/NoteItem'
+  
 import './index.scss'
 
 class index extends Component { 
@@ -35,41 +34,9 @@ class index extends Component {
 
   render() {
     const noteListRender = [1, 2].map(item => {
+      const key = item.key
       return (
-        <View className='note-item' key={item}>
-          <Location onlyShow />
-          <View className='note-main'>
-            <View className='note-line-details'>
-              <View className='note-detail-item'>
-                <View className='note-detail-item-label'>品类</View>
-                <View className='note-detail-item-content'>1111</View>
-              </View>
-              <View className='note-detail-item'>
-                <View className='note-detail-item-label'>品牌</View>
-                <View className='note-detail-item-content'>1111</View>
-              </View>
-            </View>
-            <View className='note-line-details'>
-              <View className='note-detail-item'>
-                <View className='note-detail-item-label'>价格</View>
-                <View className='note-detail-item-content'>111</View>
-              </View>
-              <View className='note-detail-item'>
-                <View className='note-detail-item-label'>型号</View>
-                <View className='note-detail-item-content'>1111</View>
-              </View>
-            </View>
-            <View className='note-line-details'>
-              <View className='note-detail-item'>
-                <View className='note-detail-item-label'>记录时间</View>
-                <View className='note-detail-item-content'>11111</View>
-              </View>
-            </View>
-            <View className='upload'>
-              
-            </View>
-          </View>
-        </View>
+        <NoteItem item={item} key={key} />
       )
     })
     return (
@@ -79,6 +46,7 @@ class index extends Component {
         home
       >
         <View className='page-wrapper'>
+          <View className=''></View>
           {
             noteListRender
           }
