c9fb60cb39a2d71970c19a05493a6b4bbaaa92e2
liuyang <liuy@paoche56.com>
2020-06-19 09:31:18 +0800

    应用安全区域组件

--- src/pages/note_publish/index.js
+++ src/pages/note_publish/index.js
@@ -4,7 +4,7 @@
  * @path: 引入路径
  * @Date: 2020-06-17 11:08:45
  * @LastEditors: liuYang
- * @LastEditTime: 2020-06-18 18:27:19
+ * @LastEditTime: 2020-06-19 09:29:47
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  * @emitFunction: 函数
@@ -17,9 +17,8 @@ import {
 import { connect } from '@tarojs/redux'
 import Upload from '@components/Upload'
 import Location from '@components/Location'
-import Nav from '@components/Nav'
+import SaveAreaView from '@components/SafeAreaView'
 import FormItem from './components/FormItem'
-
 import './index.scss'
 
 class NotePublish extends Component { 
@@ -113,117 +112,111 @@ class NotePublish extends Component {
       price,
       remark
     } = this.state
-    const {system} = this.props
-    const navHeight = system && system.navHeight || 120
     return (
-      <View
-        style={{ paddingTop: navHeight + 'rpx' }}
-        className='page-wrapper'
-      >
-        <Nav
-          title='记笔记'
-        />
-        <View className='goods-image-wrapper'>
-          <View className='goods-image-list-wrapper'>
-            <Upload
-              imageList={goodsImageList}
-              autoChoose
-              imageSizeType='big'
-              addBtnSizeType='small'
-              showAddBtn
-              alignType='flex_end'
-              onUploadOK={this.onGoodsImageUpload.bind(this)}
+      <SaveAreaView title='记笔记' back home >
+        <View className='page-wrapper'>
+          <View className='goods-image-wrapper'>
+            <View className='goods-image-list-wrapper'>
+              <Upload
+                imageList={goodsImageList}
+                autoChoose
+                imageSizeType='big'
+                addBtnSizeType='small'
+                showAddBtn
+                alignType='flex_end'
+                onUploadOK={this.onGoodsImageUpload.bind(this)}
+              />
+            </View>
+            <View className='goods-image-tips'>瓷砖类商品照片要注意拍摄光线哦~</View>
+          </View>
+          {
+            this.renderTitle('您看中的产品信息')
+          }
+          <View className='form-wrapper'>
+            <FormItem
+              line
+              important
+              label='品类'
+              unit='for-right'
+              value={category}
+              canInput={false}
+              placeholder='请选择'
+              onInput={this.onCategoryInput.bind(this)}
+            />
+            <FormItem
+              line
+              important
+              unit='text'
+              label='价格'
+              value={price}
+              unitContent='/每片'
+              placeholder='请输入'
+              onInput={this.onPriceInput.bind(this)}
+            />
+            <FormItem
+              unit='show'
+              label='型号'
+              value={price}
+              placeholder='请输入'
+              onInput={this.onPriceInput.bind(this)}
             />
           </View>
-          <View className='goods-image-tips'>瓷砖类商品照片要注意拍摄光线哦~</View>
-        </View>
-        {
-          this.renderTitle('您看中的产品信息')
-        }
-        <View className='form-wrapper'>
-          <FormItem
-            line
-            important
-            label='品类'
-            unit='for-right'
-            value={category}
-            canInput={false}
-            placeholder='请选择'
-            onInput={this.onCategoryInput.bind(this)}
-          />
-          <FormItem
-            line
-            important
-            unit='text'
-            label='价格'
-            value={price}
-            unitContent='/每片'
-            placeholder='请输入'
-            onInput={this.onPriceInput.bind(this)}
-          />
-          <FormItem
-            unit='show'
-            label='型号'
-            value={price}
-            placeholder='请输入'
-            onInput={this.onPriceInput.bind(this)}
-          />
-        </View>
-        <View className='form-wrapper card-wrapper'>
-          <View className='form-card-item'>
-            <View className='form-card-label'>价签</View>
-            <View className='form-card-content'>
-              <Upload
-                showAddBtn
-                alignType='center'
-                imageSizeType='small'
-                addBtnSizeType='big'
-                computedWidth='148'
-                imageList={priceTagImageList}
-                onUploadOK={this.onPriceTagUpload.bind(this)}
-              />
+          <View className='form-wrapper card-wrapper'>
+            <View className='form-card-item'>
+              <View className='form-card-label'>价签</View>
+              <View className='form-card-content'>
+                <Upload
+                  showAddBtn
+                  alignType='center'
+                  imageSizeType='small'
+                  addBtnSizeType='big'
+                  computedWidth='148'
+                  imageList={priceTagImageList}
+                  onUploadOK={this.onPriceTagUpload.bind(this)}
+                />
+              </View>
+            </View>
+            <View className='line'></View>
+            <View className='form-card-item'>
+              <View className='form-card-label'>名片</View>
+              <View className='form-card-content'>
+                <Upload
+                  showAddBtn
+                  alignType='center'
+                  imageSizeType='small'
+                  addBtnSizeType='big'
+                  computedWidth='148'
+                  imageList={idCardImageList}
+                  onUploadOK={this.onIdCardImageUpload.bind(this)}
+                />
+              </View>
             </View>
           </View>
-          <View className='line'></View>
-          <View className='form-card-item'>
-            <View className='form-card-label'>名片</View>
-            <View className='form-card-content'>
-              <Upload
-                showAddBtn
-                alignType='center'
-                imageSizeType='small'
-                addBtnSizeType='big'
-                computedWidth='148'
-                imageList={idCardImageList}
-                onUploadOK={this.onIdCardImageUpload.bind(this)}
-              />
-            </View>
+          {
+            this.renderTitle('门店地址')
+          }
+          <Location
+            address={address}
+            onGetLocationData={this.onGetLocationData.bind(this)}
+          />
+          {
+            this.renderTitle('备注')
+          }
+          <View className='remark-wrapper'>
+            <Textarea
+              className='textarea'
+              placeholderClass='placeholder-class'
+              autoHeight
+              placeholder='记笔记…'
+              value={remark}
+              maxlength={500}
+            ></Textarea>
+          </View>
+          <View className='bottom-wrapper'>
+            <View className='button'>保存笔记</View>
           </View>
         </View>
-        {
-          this.renderTitle('门店地址')
-        }
-        <Location
-          address={address}
-          onGetLocationData={this.onGetLocationData.bind(this)}
-        />
-        {
-          this.renderTitle('备注')
-        }
-        <View className='remark-wrapper'>
-          <Textarea
-            className='textarea'
-            placeholderClass='placeholder-class'
-            autoHeight
-            placeholder='记笔记…'
-            value={remark}
-            maxlength={500}
-          ></Textarea>
-        </View>
-        <View className='bottom-wrapper'>
-          <View className='button'>保存笔记</View>
-        </View>
-      </View>
+      </SaveAreaView>
     )
   }
 }
@@ -231,7 +224,6 @@ class NotePublish extends Component {
 const mapStateToProps = (state) => {
   return {
     userInfo: state.user_msg.userInfo,
-    system: state.system.systemInfo
   }
 }
 
