401c7d0a35f17c8ace360fa5d4bef6b178576d81
liuyang <liuy@paoche56.com>
2020-06-20 23:44:25 +0800

    优化

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -4,7 +4,7 @@
  * @path: @components/input
  * @Date: 2020-06-18 14:44:05
  * @LastEditors: liuYang
- * @LastEditTime: 2020-06-18 15:41:44
+ * @LastEditTime: 2020-06-20 22:55:12
  * @mustParam: 必传参数
  *  文档参照 https: //developers.weixin.qq.com/miniprogram/dev/component/input.html
  *  没有封装进去的方法请自行拓展
@@ -21,19 +21,6 @@ import './index.scss'
 
 export default class LickInput extends Component { 
 
-  // static options = {
-    // addGlobalClass: true // 允许外部样式修改组件样式
-  // }
-
-  constructor(props) {
-    super(props)
-    this.state={}
-  }
-
-  componentDidMount() {
-  }
-
-
   render() {
     const {
       placeholder,
@@ -48,6 +35,7 @@ export default class LickInput extends Component {
     const lickInputClassName = classNames('input-style', {
       'placeholder-class': !value
     })
+    const valueText = value.length > 20 ? value.substr(0, 20) + '...' : value
     return (
       <View className='input-wrapper'>
         {
@@ -65,7 +53,7 @@ export default class LickInput extends Component {
               placeholderClass='placeholder-class'
             ></Input>
             ): (
-              <View className={lickInputClassName}>{value || placeholder}</View>
+              <View className={lickInputClassName}>{valueText || placeholder}</View>
             )
         }
       </View>
