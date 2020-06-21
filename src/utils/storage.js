60bb2591067cd22676717c3b6e4b3ed85e7ba663
liuyang <liuy@paoche56.com>
2020-06-21 10:38:33 +0800

    加注释

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -4,14 +4,14 @@
  * @path: import { getStorage, setStorage, removeStorage } from '@utils/storage'
  * @Date: 2020-06-18 10:55:20
  * @LastEditors: liuYang
- * @LastEditTime: 2020-06-20 15:10:07
+ * @LastEditTime: 2020-06-21 09:37:45
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  * @emitFunction: 函数
  */ 
 import Taro from '@tarojs/taro'
 /**
-* 缓存数据
+* 缓存数据 建议以下划线方式编写key  如 name_name
 * @param {String} key 要缓存数据的key
 * @param {Any} value 要缓存的数据
 * @return void
@@ -20,7 +20,7 @@ export const setStorage = (key, value) => {
   Taro.setStorageSync(`luwu_${key}`, JSON.stringify(value))
 }
 /**
-* 获取缓存
+* 获取缓存 
 * @param {String} key 要获取的缓存的key
 * @return void
 */
