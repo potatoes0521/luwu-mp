c91595249b4e9538a8808b75bdc65b13f045e890
liuyang <liuy@paoche56.com>
2020-06-21 10:36:05 +0800

    备注

--- src/pages/note_details/index.js
+++ src/pages/note_details/index.js
@@ -4,16 +4,24 @@
  * @path: 引入路径
  * @Date: 2020-06-17 11:08:09
  * @LastEditors: liuYang
- * @LastEditTime: 2020-06-17 11:13:14
+ * @LastEditTime: 2020-06-21 10:35:52
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  * @emitFunction: 函数
  */ 
 import Taro, { Component } from '@tarojs/taro'
-import { View } from '@tarojs/components'
-import classNames from 'classnames'
+import {
+  View,
+  Text
+} from '@tarojs/components'
 import { connect } from '@tarojs/redux'
-import api from '@api/index.js'
+import SafeAreaView from '@components/SafeAreaView'
+import Login from '@utils/login'
+import { getNoteDetails } from '@services/modules/note'
+import Location from '@components/Location'
+import goodsState from '@config/noteGoodsKey'
+import NoteFromMain from '@/note_components/NoteFormMain'
+import ImageSwiper from './components/Swiper'
 
 import './index.scss'
 
@@ -21,19 +29,62 @@ class index extends Component {
 
   constructor(props) {
     super(props)
-    this.state={}
+    this.state = Object.assign({}, goodsState, {
+      // 除去公共key以外的字段定在这里
+    })
+    this.pageParams = {}
   }
 
-  componentDidMount() {
+  async componentDidMount() { 
+    this.pageParams = this.$router.params
+    const {userInfo} = this.props
+    !userInfo.token && await Login.login()
+    this.getNoteDetails()
+  } 
+  getNoteDetails() { 
+    getNoteDetails({
+      noteId: this.pageParams.noteId
+    }).then((res) => {
+      const json = Object.assign({}, res.data)
+      delete res['data']
+      const data = Object.assign({}, res, json)
+      this.setState(data)
+    })
   }
 
   config = {
-    navigationBarTitleText: '笔记详情' 
+    navigationBarTitleText: '笔记详情',
+    navigationStyle: 'custom'
   }
 
   render() {
+    const {
+      address,
+      goodsImageList
+    } = this.state
     return (
-      <View></View>
+      <SafeAreaView
+        title='的笔记'
+        back
+        home
+      >
+        <View className='page-wrapper'>
+          <Location onlyShow address={address} >
+            <View className='tips'>
+              <Text className='tips-text' onClick={this.HandleEditData.bind(this)}>编辑</Text>
+            </View>
+          </Location>
+          <View className='details-main-wrapper'>
+            <View className='details-swiper-wrapper'>
+              <ImageSwiper imageList={goodsImageList} />
+            </View>
+            <NoteFromMain
+              showRemark
+              item={this.state}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
     )
   }
 
