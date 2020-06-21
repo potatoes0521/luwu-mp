5a2c965460cbec490676f0e4139286d322f36c87
liuyang <liuy@paoche56.com>
2020-06-21 11:52:29 +0800

    新增随机数

--- src/utils/numberToCode.js
+++ src/utils/numberToCode.js
@@ -4,13 +4,12 @@
  * @path: 引入路径
  * @Date: 2020-06-21 10:57:24
  * @LastEditors: liuYang
- * @LastEditTime: 2020-06-21 10:57:53
+ * @LastEditTime: 2020-06-21 11:45:27
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  * @emitFunction: 函数
  */
 
-// eslint-disable-next-line import/prefer-default-export
 export const SectionToChinese = (section) => {
   var chnNumChar = ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"];
   var chnUnitChar = ["", "十", "百", "千", "万", "亿", "万亿", "亿亿"];
@@ -35,4 +34,8 @@ export const SectionToChinese = (section) => {
     section = Math.floor(section / 10);
   }
   return chnStr;
+}
+
+export const random = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
