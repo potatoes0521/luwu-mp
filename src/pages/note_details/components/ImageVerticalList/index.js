383d2a7cc48d89675895e92a9cac1d63f50baa25
liuyang <liuy@paoche56.com>
2020-06-21 11:00:46 +0800

    下标处理为汉字

---
+++ src/pages/note_details/components/ImageVerticalList/index.js
@@ -0,0 +1,61 @@
+/*
+ * @Author: liuYang
+ * @description: 请填写描述信息
+ * @path: 引入路径
+ * @Date: 2020-06-21 10:40:32
+ * @LastEditors: liuYang
+ * @LastEditTime: 2020-06-21 11:00:11
+ * @mustParam: 必传参数
+ * @optionalParam: 选传参数
+ * @emitFunction: 函数
+ */ 
+
+import Taro, { Component } from '@tarojs/taro'
+import {
+  View,
+  Image
+} from '@tarojs/components'
+import PropTypes from 'prop-types'
+import {SectionToChinese} from '@utils/numberToCode'
+import './index.scss'
+
+export default class ImageVerticalList extends Component { 
+
+  render() {
+    let { imageList, title } = this.props
+    const imageListRender = imageList.map((item, index) => {
+      const itemTitle = title ? (imageList.length > 1 ? title + SectionToChinese(index) : title) : ''
+      return (
+        <View className='image-item' key={item}>
+          <View className='image-wrapper'>
+            <Image
+              lazy-load
+              src={item}
+              mode='aspectFill'
+              className='image'
+            ></Image>
+          </View>
+          <View className='title'>{itemTitle || ''}</View>
+        </View>
+      )
+    })
+    return (
+      <View className='image-vertical-wrapper'>
+        {
+          imageListRender
+        }
+      </View>
+    )
+  }
+
+}
+
+ImageVerticalList.defaultProps = {
+  imageList: [],
+  onClick: () => {console.error('onClick is not defined')}
+}
+
+ImageVerticalList.propTypes = {
+  imageList: PropTypes.array.isRequired,
+  onClick: PropTypes.func
+}
---
+++ src/utils/numberToCode.js
@@ -0,0 +1,38 @@
+/*
+ * @Author: liuYang
+ * @description: 把数字转换成对应文字
+ * @path: 引入路径
+ * @Date: 2020-06-21 10:57:24
+ * @LastEditors: liuYang
+ * @LastEditTime: 2020-06-21 10:57:53
+ * @mustParam: 必传参数
+ * @optionalParam: 选传参数
+ * @emitFunction: 函数
+ */
+
+// eslint-disable-next-line import/prefer-default-export
+export const SectionToChinese = (section) => {
+  var chnNumChar = ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"];
+  var chnUnitChar = ["", "十", "百", "千", "万", "亿", "万亿", "亿亿"];
+  var strIns = '',
+    chnStr = '';
+  var unitPos = 0;
+  var zero = true;
+  while (section > 0) {
+    var v = section % 10;
+    if (v === 0) {
+      if (!zero) {
+        zero = true;
+        chnStr = chnNumChar[v] + chnStr;
+      }
+    } else {
+      zero = false;
+      strIns = chnNumChar[v];
+      strIns += chnUnitChar[unitPos];
+      chnStr = strIns + chnStr;
+    }
+    unitPos++;
+    section = Math.floor(section / 10);
+  }
+  return chnStr;
+}
