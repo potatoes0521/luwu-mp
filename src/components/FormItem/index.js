ff6f13cc091945b8e60174ff07e8af448c7a81b1
liuyang <liuy@paoche56.com>
2020-06-24 13:05:29 +0800

    优化

--- src/components/FormItem/index.js
+++ src/components/FormItem/index.js
@@ -4,7 +4,7 @@
  * @path: 引入路径
  * @Date: 2020-06-18 15:17:57
  * @LastEditors: liuYang
- * @LastEditTime: 2020-06-23 18:32:01
+ * @LastEditTime: 2020-06-24 12:04:06
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  *  封装了Input样式 文档参照 https: //developers.weixin.qq.com/miniprogram/dev/component/input.html
@@ -32,7 +32,8 @@ export default class FormItem extends Component {
   static options = {
     addGlobalClass: true // 允许外部样式修改组件样式
   }
-  handleContentClick(){
+  handleContentClick(e) {
+    e.stopPropagation();
     if (!this.props.canInput) {
       this.props.onContentClick()
     }
