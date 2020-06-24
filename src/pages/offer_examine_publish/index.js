0c5a629020bf677993a0c7e75cfd5454a491a6d1
liuyang <liuy@paoche56.com>
2020-06-24 13:05:55 +0800

    选择类型完成

--- src/pages/offer_examine_publish/index.js
+++ src/pages/offer_examine_publish/index.js
@@ -1,11 +1,13 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Textarea } from '@tarojs/components'
-// import classNames from 'classnames'
+import classNames from 'classnames'
 import { connect } from '@tarojs/redux'
 // import {} from '@services/modules/index'
 import SafeAreaView from '@components/SafeAreaView'
 import FormItem from '@components/FormItem'
 import Login from '@utils/login'
+import { uploadImage, uploadFile } from '@components/Upload/utils/upload_type'
+import OfferState from '@config/offerExamine'
 
 import './index.scss'
 
@@ -13,16 +15,96 @@ class OfferExaminePublish extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {
-      fileList: [1,2,3,4,5,6]
-    }
+    this.state = Object.assign({}, OfferState, {
+      // 除去公共key以外的字段定在这里
+      openModelModal: false,
+      
+    })
   }
 
   async componentDidMount() {
     const {userInfo} = this.props
     !userInfo.token && await Login.login()
   }
-  handleClickModel() { 
+  handleClickModel() {
+    const { openModelModal } = this.state
+    this.setState({
+      openModelModal: !openModelModal
+    })
+  }
+  onClickModelModalItem(item, e) { 
+    e.stopPropagation();
+    console.log('item', item)
+    this.setState({
+      model: item
+    }, () => {
+        this.handleClickModel()
+    })
+  }
+  /**
+   * 处理上传文件或图片
+   * @return void
+   */
+  handleUpload() { 
+    const chooseTypeList = ['拍照', '相册选择图片', '文件']
+    const { fileList } = this.state
+    Taro.showActionSheet({
+        itemList: chooseTypeList
+      })
+      .then(({tapIndex}) => {
+        if (tapIndex === 2) { 
+          uploadFile({ that: this }).then(res => {
+            const fileData = res.map(item => {
+              return {
+                url: item,
+                fileType: 'file'
+              }
+            })
+            this.setState({
+              fileList: [...fileList, ...fileData]
+            })
+          })
+        } else {
+          uploadImage({
+            sourceType: [tapIndex === 0 ? 'camera' : 'album'],
+          }).then(res => {
+            const fileData = res.map(item => {
+              return {
+                url: item,
+                fileType: 'image'
+              }
+            })
+            this.setState({
+              fileList: [...fileList, ...fileData]
+            })
+          })
+        }
+      })
+      .catch(err => console.log(err.errMsg))
+  }
+  /**
+   * 处理查看文件
+   * @param {Type} file 参数描述
+   * @return void
+   */
+  handleClickFile(file) { 
+    console.log('file', file)
+    if (file.fileType === 'image') { 
+      Taro.previewImage({
+        current: file.url
+      })
+    } else {
+      Taro.downloadFile({
+        url: file.url,
+        success: ({tempFilePath}) => {
+          Taro.openDocument({
+            filePath: tempFilePath
+          })
+        }
+      })
+    }
+  }
+  onRemarkInput() { 
     
   }
   /**
@@ -35,25 +117,51 @@ class OfferExaminePublish extends Component {
       <View className='title'>{title}</View>
     )
   }
+  stopPropagation(e) {
+    e.stopPropagation()
+  }
   config = {
     navigationBarTitleText: '免费申报价',
     navigationStyle: 'custom'
   }
-  onUploadOK(fileList) {
-    console.log('fileList', fileList)
-  }
   render() {
-    const { fileList } = this.state
-    const fileListRender = fileList.map(item => {
-      const key = item.url
+    const {
+      fileList,
+      model,
+      area,
+      remark,
+      mobile,
+      userName,
+      openModelModal,
+      modelModalData
+    } = this.state
+    const fileListRender = fileList.map((file, index) => {
+      const key = file.url
+      const fileName = `文件${index + 1}`
       return (
         <View
           className='file-item'
           key={key}
-        >
-          1111
-        </View>
+          onClick={this.handleClickFile.bind(this, file)}
+        >{fileName}</View>
       )
+    })
+    const modelModalRender = modelModalData.map(item => {
+      const key = item.modelId
+      const itemClassName = classNames('model-item', {
+        'model-item-active': item.modelId === model.modelId
+      })
+      return (
+        <View
+          className={itemClassName}
+          key={key}
+          onClick={this.onClickModelModalItem.bind(this, item)}
+        >{item.modelName}</View>
+      )
+    })
+    const modelFormClassName = classNames('iconRectangle', {
+      'rotated-90': openModelModal,
+      'rotated': !openModelModal
     })
     return (
       <SafeAreaView
@@ -66,16 +174,20 @@ class OfferExaminePublish extends Component {
             this.renderTitle('上传户型图和报价单')
           }
           <View className='upload-wrapper'>
-            <View className='upload-view iconfont iconbianzu'></View>
+            <View className='upload-view iconfont iconbianzu' onClick={this.handleUpload.bind(this)}></View>
             <View className='upload-tips'>文件支持jpg/png/doc/docx/pdf/xls/xlsx  ( 5MB以内 )</View>
-            <View className='upload-over-wrapper'>
-              <View className='upload-title'>已上传文件</View>
-              <View className='upload-over-list'>
-                {
-                  fileListRender
-                }
-              </View>
-            </View>
+            {
+              fileList && fileList.length && (
+                <View className='upload-over-wrapper'>
+                  <View className='upload-title'>已上传文件</View>
+                  <View className='upload-over-list'>
+                    {
+                      fileListRender
+                    }
+                  </View>
+                </View>
+              )
+            }
           </View>
           {
             this.renderTitle('您的房屋信息')
@@ -85,27 +197,49 @@ class OfferExaminePublish extends Component {
               line
               label='类型'
               unit='icon'
-              iconName='iconRectangle rotated'
+              iconName={modelFormClassName}
               shortUnit
-              value=''
+              value={model.modelName || ''}
               canInput={false}
-              placeholder='请选择'
+              placeholder={openModelModal ? '' : '请选择'}
               onContentClick={this.handleClickModel.bind(this)}
             />
+            {
+              openModelModal && (
+                <View
+                  className='model-modal-wrapper'
+                  onClick={this.stopPropagation.bind(this)}
+                >
+                  <View className='modal-main'>
+                    {
+                      modelModalRender
+                    }
+                  </View>
+                </View>
+              )
+            }
             <FormItem
               label='面积'
+              type='digit'
               unit
               shortUnit
-              value=''
+              value={area}
               placeholder='请输入'
-              onContentClick={this.handleClickModel.bind(this)}
+              canInput
             />
           </View>
           {
             this.renderTitle('补充信息')
           }
           <View className='remark-wrapper'>
-            <Textarea className='textarea'></Textarea>
+            <Textarea
+              autoHeight
+              className='textarea'
+              placeholderClass='placeholder-class'
+              value={remark}
+              onInput={this.onRemarkInput.bind(this)}
+              maxlength={500}
+            ></Textarea>
           </View>
           {
             this.renderTitle('您的个人信息')
@@ -116,18 +250,18 @@ class OfferExaminePublish extends Component {
               label='联系人'
               unit
               shortUnit
-              value=''
+              value={userName}
               placeholder='请输入'
-              onContentClick={this.handleClickModel.bind(this)}
+              canInput
             />
             <FormItem
               label='手机号码'
               type='number'
               unit
               shortUnit
-              value=''
+              value={mobile}
               placeholder='请输入'
-              onContentClick={this.handleClickModel.bind(this)}
+              canInput
             />
           </View>
           <View className='bottom-tips'>监理审完报价后会通过手机跟您联系</View>
@@ -144,7 +278,6 @@ class OfferExaminePublish extends Component {
 const mapStateToProps = (state) => {
   return {
     userInfo: state.user_msg.userInfo,
-    system: state.system.systemInfo,
   }
 }
 export default connect(mapStateToProps)(OfferExaminePublish)
