ea43c0652418723b5473a82a3a9ab9fbda329046
liuyang <liuy@paoche56.com>
2020-07-01 13:34:36 +0800

    公共样式提取

--- src/pages/choose_house_type/index.js
+++ src/pages/choose_house_type/index.js
@@ -4,7 +4,7 @@
  * @path: 引入路径
  * @Date: 2020-06-18 18:18:12
  * @LastEditors: liuYang
- * @LastEditTime: 2020-07-01 13:24:30
+ * @LastEditTime: 2020-07-01 13:28:39
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  * @emitFunction: 函数
@@ -12,7 +12,6 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
 import { connect } from '@tarojs/redux'
-import { getCategory } from '@services/modules/category'
 import SaveAreaView from '@components/SafeAreaView'
 import Login from '@utils/login'
 import { getStorage } from '@utils/storage'
@@ -34,28 +33,11 @@ class ChooseHouseType extends Component {
   }
 
   componentDidMount() {
-    this.getAllCategoryData()
     this.pageParams = this.$router.params
     const {userInfo} = this.props
     !userInfo.token && Login.login()
   }
-  /**
-   * 获取全部品类数据
-   * @return void
-   */
-  getAllCategoryData() { 
-    getCategory(this).then(res => {
-      if (!res || res.length < 1) return
-      const mainCategoriesList = res.filter(item => item.categoryLevel === 1)
-      const childCategoriesList = res.filter(item => item.categoryLevel === 2)
-      this.AllChildCategoriesList = childCategoriesList
-      this.setState({
-        mainCategoriesList
-      }, () => {
-        this.pageParams.pageType === 'edit' && this.handleEdit()
-      })
-    })
-  }
+  
   /**
    * 如果是编辑来处理编辑
    * @return void
@@ -70,71 +52,19 @@ class ChooseHouseType extends Component {
       })
     })
   }
-  /**
-   * 选择主品类
-   * @param {Object} city 选中的主品类
-   * @param {Boolean} autoSelectNext 自动选择下一个 Level
-   * @return void
-   */
-  chooseMainCategories(item, autoSelectNext) {
-    const { selectMainCategoriesData, selectChildCategoriesData } = this.state
-    if (!autoSelectNext && item.categoryId === selectMainCategoriesData.categoryId) {
-      return
-    }
-    let childCategoriesList = this.AllChildCategoriesList.filter(child => child.parentId === item.categoryId)
-    let data = {
-      childCategoriesList,
-    }
-    // autoSelectNext 为true是编辑数据  自动选择下一项  故而不清除选中项
-    if (!autoSelectNext) {
-      data.selectMainCategoriesData = item
-      data.selectChildCategoriesData = {}
-    }
-    this.setState(data, () => {
-      if (autoSelectNext) {
-        this.chooseChildCategories(selectChildCategoriesData, autoSelectNext)
-      }
-    });
-  }
-  /**
-   * 选择子品类
-   * @param {Object} city 选中的城市
-   * @param {Boolean} autoSelectNext 自动选择下一个 Level
-   * @return void
-   */
-  chooseChildCategories(item, autoSelectNext) {
-    const { selectChildCategoriesData } = this.state
-    if (!autoSelectNext && item.categoryId === selectChildCategoriesData.categoryId) {
-      return
-    }
-    let data = {}
-    // autoSelectNext 为true是编辑数据 故而不清除选中项
-    if (!autoSelectNext) {
-      data.selectChildCategoriesData = item
-    }
-    this.setState(data, () => {
-      !autoSelectNext && this.handlePrePageData()
-    });
-  }
+  
+  
   /**
    * 处理上一页数据并返回
    * @return void
    */
-  handlePrePageData() { 
+  handlePrePageData(data) { 
     let pages = Taro.getCurrentPages() //  获取页面栈
     let prevPage = pages[pages.length - 2] // 上一个页面
     if (!prevPage) {
       return
     }
-    const {
-      selectMainCategoriesData, // 选中的品类
-      selectChildCategoriesData, // 选中的子品类
-    } = this.state
-    prevPage.$component.setState({
-      mainCategory: selectMainCategoriesData,
-      childCategory: selectChildCategoriesData,
-      priceUnit: selectChildCategoriesData.unit
-    }, () => {
+    prevPage.$component.setState(data, () => {
       Taro.navigateBack()
     })
   }
@@ -192,8 +122,10 @@ class ChooseHouseType extends Component {
           className='page-wrapper'
         >
           <View className='fixed-nav'>
-            <View className='fixed-nav-text'>主品类</View>
-            <View className='fixed-nav-text'>子品类</View>
+            <View className='fixed-nav-text'>房间</View>
+            <View className='fixed-nav-text'>客厅</View>
+            <View className='fixed-nav-text'>厨房</View>
+            <View className='fixed-nav-text'>卫生间</View>
           </View>
           <View className='main-wrapper'>
             <View className='child-list-wrapper'>
