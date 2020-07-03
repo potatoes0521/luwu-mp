4d2f32c61a8891a403749e517661282edf806b64
liuyang <liuy@paoche56.com>
2020-07-03 10:46:49 +0800

    处理默认数据

--- src/config/offerExamineState.js
+++ src/config/offerExamineState.js
@@ -4,7 +4,7 @@
  * @path: 引入路径
  * @Date: 2020-06-24 10:03:17
  * @LastEditors: liuYang
- * @LastEditTime: 2020-07-03 10:13:04
+ * @LastEditTime: 2020-07-03 10:45:51
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  * @emitFunction: 函数
@@ -12,5 +12,5 @@
 import houseState from './houseState'
 
 export default Object.assign({}, houseState, {
-  fileList: [1, 2, 3, 4, 5],
+  fileList: [],
 })
